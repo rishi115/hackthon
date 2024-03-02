a2d2b7c90ac9504b4d6dad44fb6e499373eb23cb
ASUS <rishidevare051@gmail.com>
2024-03-02 23:54:05 +0530

    Added query func

--- node-server/routes.js
+++ node-server/routes.js
@@ -1,10 +1,30 @@
-const { addBlog, getBlogs } = require("./controllers/BlogController");
+const {
+  addBlog,
+  getBlogs,
+  deleteBlog,
+} = require("./controllers/BlogController");
 const { chat } = require("./controllers/OpenaiController");
+const {
+  addQuery,
+  getQueries,
+  deleteQuery,
+  replyQuery,
+} = require("./controllers/QueryController");
 
 const router = require("express").Router();
 
+// BLOG ROUTES
 router.post("/add-blog", addBlog);
 router.get("/get-blogs", getBlogs);
+router.delete("/delete-blog/:id", deleteBlog);
+
+// CHAT ROUTE
 router.post("/chat", chat);
+
+// QUERY ROUTES
+router.post("/add-query", addQuery);
+router.get("/get-queries", getQueries);
+router.delete("/delete-query/:id", deleteQuery);
+router.put("/reply-query/:id", replyQuery);
 
 module.exports = router;
